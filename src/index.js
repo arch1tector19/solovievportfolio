2e217fabf74f2e6a1f58a2d1716a6dc631cc3b8a
arch1tectot17 <arch1tectot17@bigboybottegaboy.myguest.virtualbox.org>
2025-10-11 20:23:50 +0300

    force rebuild

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css";
 import SolovievPortfolio from "./SolovievPortfolio";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<SolovievPortfolio />);
+root.render(
+  <React.StrictMode>
+    <SolovievPortfolio />
+  </React.StrictMode>
+);
+
